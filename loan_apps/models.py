246f1a8d38232ae32d79108766a63728df10835a
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-08-05 00:03:46 -0400

    model-1

--- loan_apps/models.py
+++ loan_apps/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+class Business(models.Model):
+    Name = models.CharField(max_length=100)
+    TaxID = models.IntegerField(max_length=9)
+    Phone = models.IntegerField(max_length=10)
+    NAICS = models.IntegerField(max_length=5)
+    HasBeenProfitable = models.BooleanField()
+    HasBankruptedInLast7Years = models.BooleanField()
+    InceptionDate = models.DateTimeField()
+    Address = models.OneToOneField('Address',on_delete=models.CASCADE)
+
+class Address(models.Model):
+    Address1 = models.TextField()
+    Address2 = models.TextField()
+    City = models.CharField(max_length=30)
+    State = models.CharField(max_length=2)
+    Zip = models.IntegerField(max_length=5)
+
 # Create your models here.
--- loan_apps/views.py
+++ loan_apps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("First ever view.")
+
+def second(request):
+    return HttpResponse("Second option")
 
 # Create your views here.
